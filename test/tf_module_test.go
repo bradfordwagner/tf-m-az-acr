c598dc7c03b8a41679c0657ce7695efd2a958060
bradfordwagner <wagner.bradford@gmail.com>
2023-05-17 23:25:32 -0400

    re-enable destroy

--- test/tf_module_test.go
+++ test/tf_module_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -10,10 +12,8 @@ import (
 func TestTerraformBasicExample(t *testing.T) {
 	t.Parallel()
 
-	// name := fmt.Sprintf("test-tf-m-az-acr-%d", rand.Intn(10000))
-	// resourceGroupName := fmt.Sprintf("test-rg-%d", rand.Intn(10000))
-	name := "test-tf-m-az-acr"
-	resourceGroupName := "test-rg-"
+	name := fmt.Sprintf("test-tf-m-az-acr-%d", rand.Intn(10000))
+	resourceGroupName := fmt.Sprintf("test-rg-%d", rand.Intn(10000))
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// website::tag::1::Set the path to the Terraform code that will be tested.
 		// The path to where our Terraform code is located
@@ -35,7 +35,7 @@ func TestTerraformBasicExample(t *testing.T) {
 
 	// website::tag::4::Clean up resources with "terraform destroy". Using "defer" runs the command at the end of the test, whether the test succeeds or fails.
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	// defer terraform.Destroy(t, terraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
 
 	// website::tag::2::Run "terraform init" and "terraform apply".
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
