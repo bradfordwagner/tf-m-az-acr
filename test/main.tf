e247704a2f08db4d8fd8e15635cf15af15a150fa
bradfordwagner <wagner.bradford@gmail.com>
2023-05-17 22:49:41 -0400

    add providers

--- test/main.tf
+++ test/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+
+}
+
 module "test_module" {
   source = "../"
   input  = var.input
